d3d8921bba51389f00cd5226615ab1a7ed7da04d
dekc89 <dekc89@gmail.com>
2018-11-16 17:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber(currQueue, name) {
   // return "Welcome, " + name + "." + " You are number 1 in line."
+  console.log(currQueue);
   return `Welcome, ${name}. You are number ${currQueue} in line.`
 }
