d2f3403f4775fdb97f8ad7b512dcfe616e80d4af
dekc89 <dekc89@gmail.com>
2018-11-16 17:55:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ function nowServing(queue) {
     return "There is nobody waiting to be served!";
   }
 
-  return "Currently serving " + queue.shift() + '.';
+  // return "Currently serving " + queue.shift() + '.';
+  return `Currently serving ${queue.shift()}.`
 }
