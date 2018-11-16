383bf7baa128d1c7941aafdafbe2bd9c19ad0d4f
dekc89 <dekc89@gmail.com>
2018-11-16 17:45:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function takeANumber(currQueue, name) {
   // return "Welcome, " + name + "." + " You are number 1 in line."
   console.log(currQueue);
+  katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${currQueue.length + 1} in line.`
+  
 }
